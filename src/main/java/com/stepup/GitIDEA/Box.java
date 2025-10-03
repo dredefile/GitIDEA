bccc2d2bfb13317a5a7be5e3a21a1fb82ba45dcb
Kostomarov Nikita <dredefiles@gmail.com>
2025-10-03 16:31:56 +0300

    Изменена логика и добавлен класс Box

---
+++ src/main/java/com/stepup/GitIDEA/Box.java
@@ -0,0 +1,17 @@
+package com.stepup.GitIDEA;
+
+public class Box {
+    private int size;
+
+    public Box(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
--- src/main/java/com/stepup/GitIDEA/MainApplication.java
+++ src/main/java/com/stepup/GitIDEA/MainApplication.java
@@ -1,6 +1,12 @@
+package com.stepup.GitIDEA;
+
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Меняю код для второго коммита");
+    }
+
+    public static void doSomething () {
+        System.out.println("1");
     }
 }
