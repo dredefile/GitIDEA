3588bbee60c34e4248b5b5fc8c796640ea0bd5b7
Kostomarov Nikita <dredefiles@gmail.com>
2025-10-05 16:40:03 +0300

    Изменили логику main и doSomething

--- src/main/java/com/stepup/GitIDEA/MainApplication.java
+++ src/main/java/com/stepup/GitIDEA/MainApplication.java
@@ -2,13 +2,14 @@ package com.stepup.GitIDEA;
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("------");
         System.out.println("Hello World!");
         System.out.println("Меняю код для второго коммита");
     }
 
     public static void doSomething () {
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("0");
+        System.out.println("0");
     }
 }
